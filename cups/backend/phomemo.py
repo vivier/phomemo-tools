vivier/phomemo-tools

commit 556d84d351997534a63193ca52ac148aaf123370

Make usb import optional

cups/backend/phomemo.py
@@ -5,8 +5,6 @@ import os
 import subprocess
 import dbus
 import socket
-import usb.core
-import usb.util
 
 bus = dbus.SystemBus()
 
@@ -58,6 +56,12 @@ class find_class(object):
         return False
 
 def scan_usb():
+    try:
+        import usb.core
+        import usb.util
+    except ModuleNotFoundError:
+        print("WARNING: Please install python3-usb to support usb-discovery", file=sys.stderr)
+        return
     printers = usb.core.find(find_all=1, custom_match=find_class(7), idVendor=0x0493)
     for printer in printers:
             for cfg in printer:
@@ -78,6 +82,7 @@ def scan_usb():
             device_make_and_model = 'Phomemo ' + model
             print('direct ' + device_uri + ' "' + device_make_and_model + '" "' +
               device_make_and_model + ' USB ' + SerialNumber + '" "' + device_id + model + ' (USB);"')
+
 
 if len(sys.argv) == 1:
     scan_bluetooth()

commit accf0b5d3df4694bfe571be7df7ef9a23803466d

backend: fix USB discovery
Move usb import from scan_usb() to main part to fix the following error:

    File /cups/backend/phomemo.py, line 54, in __call__
      intf = usb.util.find_descriptor(
  NameError: name 'usb' is not defined

Fixes: 556d84d35199 ("Make usb import optional")
Signed-off-by: Laurent Vivier <laurent@vivier.eu>

cups/backend/phomemo.py
@@ -61,12 +61,6 @@ class find_class(object):
         return False
 
 def scan_usb():
-    try:
-        import usb.core
-        import usb.util
-    except ModuleNotFoundError:
-        print("WARNING: Please install python3-usb to support usb-discovery", file=sys.stderr)
-        return
     printers = usb.core.find(find_all=1, custom_match=find_class(7), idVendor=0x0493)
     for printer in printers:
             for cfg in printer:
@@ -91,6 +85,12 @@ def scan_usb():
 
 if len(sys.argv) == 1:
     scan_bluetooth()
+    try:
+        import usb.core
+        import usb.util
+    except ModuleNotFoundError:
+        print("WARNING: Please install python3-usb to support usb-discovery", file=sys.stderr)
+        exit(0)
     scan_usb()
     exit(0)
 